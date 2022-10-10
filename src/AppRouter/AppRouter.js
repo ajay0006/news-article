23e13493bce8cb8767688fb27d6f817a12c6c71a
damilola <ajay0006@algonquinlive.com>
2022-10-10 00:38:16 -0400

    configuring the main layout

--- src/AppRouter/AppRouter.js
+++ src/AppRouter/AppRouter.js
@@ -1,5 +1,8 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MainLayout from "../layouts/mainLayout";
+import Header from "../components/header";
+import Footer from "../components/footer";
 import Home from "../components/home";
 import Contact from "../components/contact";
 import Posts from "../components/posts";
@@ -7,13 +10,15 @@ import Posts from "../components/posts";
 
 const AppRouter = () => (
     <BrowserRouter>
-        <>
+        <Header/>
+        <MainLayout>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="contact" element={<Contact/>}/>
                 <Route path="article/:id" element={<Posts/>}/>
             </Routes>
-        </>
+        </MainLayout>
+        <Footer/>
     </BrowserRouter>
 );
 
