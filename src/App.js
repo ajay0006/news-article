e2344fa53c2b3cb371a26a622899879f08e70fb9
damilola <ajay0006@algonquinlive.com>
2022-10-09 23:59:04 -0400

    created component and routes

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import AppRouter from "./AppRouter/AppRouter";
+const App = () => {
+    return(
+        <>
+            <AppRouter/>
+        </>
+    )
+}
+
+export default App;
---
+++ src/AppRouter/AppRouter.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "../components/home";
+import Contact from "../components/contact";
+import Posts from "../components/posts";
+
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="contact" element={<Contact/>}/>
+                <Route path="posts" element={<Posts/>}/>
+            </Routes>
+        </>
+    </BrowserRouter>
+);
+
+export default AppRouter;
